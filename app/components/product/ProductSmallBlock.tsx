6599124db2a5432d698f727d4ed9e04a01927144
Tejpal Singh <mca.t2911@gmail.com>
2022-07-06 19:14:32 +0530

    plp components breakdown

---
+++ app/components/product/ProductSmallBlock.tsx
@@ -0,0 +1,86 @@
+import React0 from "react";
+
+const ProductSmallBlock = (props: any) => {
+    return (
+        <div className="col-xl-6">
+            <div className={"product-block bg-"+props.bg_type}>
+                <div className="row g-0">
+                    <div className="col-sm-7">
+                        <div className="product-image position-relative">
+                            <img
+                                src={props.image}
+                                className="img-fluid"
+                            />
+                            <button type="button" className="btn-voice">
+                                <i className="fas fa-volume-high fa-fw" />
+                            </button>
+                            <button type="button" className="btn-heart">
+                                <i className="far fa-heart fa-fw" />
+                            </button>
+                            <span className="img-name">Cotton + Khaadi</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-5">
+                        <div className="product-content-area">
+                            <h6>{props.model_name}</h6>
+                            <p>{props.name}</p>
+                            <div className="product-rating text-center">
+                                <span>4.6</span>
+                                <ul className="list-inline">
+                                    <li className="list-inline-item">
+                                        <i className="fas fa-star fa-fw" />
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <i className="fas fa-star fa-fw" />
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <i className="fas fa-star fa-fw" />
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <i className="fas fa-star fa-fw" />
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <i className="fas fa-star-half fa-fw" />
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="product-price text-center">
+                                <span className="less-price">
+                                    <i className="fas fa-indian-rupee-sign fa-fw" />{" "}
+                                    6,450
+                                </span>
+                                <span className="total-price">
+                                    <i className="fas fa-indian-rupee-sign fa-fw" />{" "}
+                                    3,450
+                                </span>
+                                <span className="less-save">
+                                    You save{" "}
+                                    <i className="fas fa-indian-rupee-sign fa-fw" />{" "}
+                                    3,000{" "}
+                                </span>
+                            </div>
+                            <div className="product-btn text-center">
+                                <a
+                                    href="#"
+                                    className="btn-outline btn-sm w-100 mb-2"
+                                >
+                                    Quick View
+                                </a>
+                                <button
+                                    type="button"
+                                    className="btn btn-sm w-100 cart-btn"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal"
+                                >
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    )
+}
+
+export default ProductSmallBlock;
