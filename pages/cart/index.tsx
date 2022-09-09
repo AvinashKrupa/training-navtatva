dd083a879465d9e1d570bfff671cb613c638e342
Yatish Agrawal <yatish.agrawal@mqspectrum.com>
2022-09-09 15:29:06 +0530

    fixed cart isue

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -95,7 +95,9 @@ const CartScreen: NextPage = () => {
           )}
 
           <div className="row">
-            <div className="col-md-12 col-lg-8">
+            <div
+              className={`col-md-12 col-lg-${cartItems?.length != 0 ? 8 : 12}`}
+            >
               {loading && <CartItemLoader />}
               {cartItems?.length != 0 &&
                 cartItems?.map((item: any, index: number) => {
@@ -122,116 +124,119 @@ const CartScreen: NextPage = () => {
                 </>
               )}
             </div>
-            <div className="col-md-12 col-lg-4">
-              <div className="w-100 mt-4">
-                <a href="#">
-                  <img className="w-100" src="images/discountAd.png" alt="" />
-                </a>
-                <a
-                  href="button"
-                  className="font-sb text-color-3 fs-16 justify-content-end align-items-end d-flex ms-auto mt-3 me-2"
-                >
-                  Multiple offers waiting for you after checkout
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={22}
-                    height={22}
-                    fill="currentColor"
-                    className="bi bi-arrow-up-right ms-2"
-                    viewBox="0 0 16 16"
+            {cartItems?.length != 0 && (
+              <div className="col-md-12 col-lg-4">
+                <div className="w-100 mt-4">
+                  <a href="#">
+                    <img className="w-100" src="images/discountAd.png" alt="" />
+                  </a>
+                  <a
+                    href="button"
+                    className="font-sb text-color-3 fs-16 justify-content-end align-items-end d-flex ms-auto mt-3 me-2"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"
-                    />
-                  </svg>
-                </a>
-              </div>
-              <div className="bg-white border p-3 mt-4 shipping">
-                <div className="row">
-                  <div className="col-md-12 py-2">
-                    <p className="fs-19 font-r text-color-2">
-                      <span className="font-sb">2 of 4 Items</span> selected for
-                      checkout
-                    </p>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="gray">
-                      <p className="fs-14 font-r text-color-2">
-                        Add items worth
-                        <span className="font-sb"> ₹1,800 </span> more to avail
-                        <span className="font-sb text-color-9">
-                          Free Shipping
-                        </span>
+                    Multiple offers waiting for you after checkout
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={22}
+                      height={22}
+                      fill="currentColor"
+                      className="bi bi-arrow-up-right ms-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+                <div className="bg-white border p-3 mt-4 shipping">
+                  <div className="row">
+                    <div className="col-md-12 py-2">
+                      <p className="fs-19 font-r text-color-2">
+                        <span className="font-sb">2 of 4 Items</span> selected
+                        for checkout
                       </p>
                     </div>
-                  </div>
-                  <div className="col-md-12 d-flex mt-5">
-                    <h3 className="fs-19 font-sb text-color-2">Sub Total</h3>
-                    <h3 className="fs-24 font-sb text-color-3 ms-auto">
-                      ₹{subTotal}
-                    </h3>
-                  </div>
-                  <div className="col-md-12 mt-4">
-                    <a
-                      href="/checkout"
-                      className="btn fs-18 w-100"
-                      tabIndex={0}
-                    >
-                      Proceed to Checkout
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-12 mt-4">
-                  <div className="mb-3 promocode ">
-                    <label className="col-form-label fs-14 font-sb text-color-1">
-                      Have a Promo Code?
-                    </label>
-                    <div className="col-sm-12 position-relative">
-                      <input
-                        value={couponCode}
-                        onChange={(event) => {
-                          setCouponCode(event.target.value);
-                        }}
-                        type="text"
-                        className="form-control"
-                      />
+                    <div className="col-md-12">
+                      <div className="gray">
+                        <p className="fs-14 font-r text-color-2">
+                          Add items worth
+                          <span className="font-sb"> ₹1,800 </span> more to
+                          avail
+                          <span className="font-sb text-color-9">
+                            Free Shipping
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="col-md-12 d-flex mt-5">
+                      <h3 className="fs-19 font-sb text-color-2">Sub Total</h3>
+                      <h3 className="fs-24 font-sb text-color-3 ms-auto">
+                        ₹{subTotal}
+                      </h3>
+                    </div>
+                    <div className="col-md-12 mt-4">
                       <a
-                        href="#"
-                        className=" fs-16 font-sb text-color-3 text-end apply"
-                        onClick={(event) => {
-                          event.preventDefault();
-                          applyCouponCode();
-                        }}
+                        href="/checkout"
+                        className="btn fs-18 w-100"
+                        tabIndex={0}
                       >
-                        Apply
+                        Proceed to Checkout
                       </a>
                     </div>
                   </div>
                 </div>
-                <div className="col-md-12 mt-4">
-                  <label className="col-form-label fs-14 font-sb text-color-1 mb-2">
-                    Have a Promo Code?
-                  </label>
-                  <a href="#">
-                    <img className="w-100" src="images/card-1.png" alt="" />
-                  </a>
-                  <a href="#" className="mt-4 d-block">
-                    <img className="w-100" src="images/card-2.png" alt="" />
-                  </a>
-                </div>
-                <div className="col-md-12 mt-4">
-                  <label className="col-form-label fs-14 font-sb text-color-1 mb-2">
-                    Redeem Coins
-                  </label>
-                  <a href="#">
-                    <img className="w-100" src="images/card-3.png" alt="" />
-                  </a>
+                <div className="row">
+                  <div className="col-md-12 mt-4">
+                    <div className="mb-3 promocode ">
+                      <label className="col-form-label fs-14 font-sb text-color-1">
+                        Have a Promo Code?
+                      </label>
+                      <div className="col-sm-12 position-relative">
+                        <input
+                          value={couponCode}
+                          onChange={(event) => {
+                            setCouponCode(event.target.value);
+                          }}
+                          type="text"
+                          className="form-control"
+                        />
+                        <a
+                          href="#"
+                          className=" fs-16 font-sb text-color-3 text-end apply"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            applyCouponCode();
+                          }}
+                        >
+                          Apply
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-12 mt-4">
+                    <label className="col-form-label fs-14 font-sb text-color-1 mb-2">
+                      Have a Promo Code?
+                    </label>
+                    <a href="#">
+                      <img className="w-100" src="images/card-1.png" alt="" />
+                    </a>
+                    <a href="#" className="mt-4 d-block">
+                      <img className="w-100" src="images/card-2.png" alt="" />
+                    </a>
+                  </div>
+                  <div className="col-md-12 mt-4">
+                    <label className="col-form-label fs-14 font-sb text-color-1 mb-2">
+                      Redeem Coins
+                    </label>
+                    <a href="#">
+                      <img className="w-100" src="images/card-3.png" alt="" />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </section>
         <VisitNunchiBanner />
