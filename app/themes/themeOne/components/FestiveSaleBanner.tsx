25ddecd148b9024ce9250fcda20e9c9d0b69bfac
Srikanth <srikanth.r@cnetric.com>
2022-09-12 12:50:37 +0530

    Update FestiveSaleBanner.tsx

--- app/themes/themeOne/components/FestiveSaleBanner.tsx
+++ app/themes/themeOne/components/FestiveSaleBanner.tsx
@@ -20,7 +20,7 @@ const FestiveSaleBanner = () => {
         CatalogService.getInstance()
             .getBanner()
             .then((info: any) => {
-                //setBannerUrl(info?.data?.data[2].attributes.image.data.attributes.url);
+                setBannerUrl(info?.data?.data[0].attributes.image.data.attributes.url);
                 setLoading(false)
                 console.log("this is Banner", info)
                 setBanners(info?.data?.data)
@@ -38,7 +38,7 @@ const FestiveSaleBanner = () => {
                                 return (
                                     <div key={index}>
                                         <div className="col-md-12 col-lg-12" >
-                                            <img  className="w-100" src={`${constants.assetsBaseURL}${items.attributes.image.data.attributes.url}`} alt="" />
+                                            <img  className="w-100" src={`${constants.assetsBaseURL}${bannerUrl}`} alt="" />
                                         </div>
                                     </div>)
                             })}
