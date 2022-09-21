aaf11939314868ac62a5b3a4e35b5fbc5298db1b
Tejpal_Singh <tejpal@cnetric.com>
2022-09-21 18:57:00 +0530

    progress njs

--- pages/wishlist/index.tsx
+++ pages/wishlist/index.tsx
@@ -20,6 +20,7 @@ const WishlistScreen: NextPage = () => {
         } else {
             setLoginPopup(true);
         }
+        //deleteWishListEntry();
     }, [isLogin]);
 
     function getAllWishist() {
@@ -40,7 +41,18 @@ const WishlistScreen: NextPage = () => {
             .catch((error) => { });
     }
 
-    console.log("this is", wishlistProductIds)
+    function deleteWishListEntry() {
+        let entries: any =[];
+        entries.map( (item: any) => {
+            Wishlist.getInstance()
+            .deleteWishListEntry(item.id)
+            .then((data: any) => {
+                console.log("wishlist entry delted", data)
+            })
+            .catch((error) => console.log(error));
+        })
+        return true;
+    }
     return (
         <div className="mycollection">
             <div className="wrapper">
